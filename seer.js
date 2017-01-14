dd24bf6aef970570f8030505c104a2c4d627d569
Damian Dulisz <damian.dulisz@monterail.com>
2017-01-14 18:02:02 +0100

    Add dependency tracking

--- seer.js
+++ seer.js
@@ -1,45 +1,63 @@
-function Seer (initObj) {
+function Seer (dataObj) {
   let signals = {}
-
-  // observeComputed(initObj.computed, initObj.data)
-  observeData(initObj.data)
-
-  return {
-    observe,
-    notify,
-    data: initObj.data
+  let Dep = {
+    target: null
   }
 
-  function observe (signal, signalHandler) {
-    if(!signals[signal]) signals[signal] = []
+  observeData(dataObj)
 
-    signals[signal].push(signalHandler)
+  return {
+    data: dataObj,
+    observe,
+    notify
+  }
+
+  function observe (property, signalHandler) {
+    if(!signals[property]) signals[property] = []
+
+    signals[property].push(signalHandler)
   }
 
   function notify (signal) {
     if(!signals[signal] || signals[signal].length < 1) return
 
-    signals[signal].forEach((signalHandler) => signalHandler())
+    signals[signal].forEach(signalHandler => signalHandler())
   }
 
-  function syncDOM (obj, node, observableName) {
-    node.textContent = obj[observableName]
-    observe(observableName, value => node.textContent = obj[observableName] || '')
-  }
+  function makeReactive (obj, key, computeFunc) {
+    let deps = []
+    let val = obj[key]
 
-  function parseDOM (node, observable) {
-    const nodes = document.querySelectorAll('[sync]')
-    nodes.forEach((node) => {
-      syncDOM(observable, node, node.attributes['sync'].value)
+    Object.defineProperty (obj, key, {
+      get () {
+        if (computeFunc) {
+          if (!Dep.target) Dep.target = key
+          val = computeFunc.call(obj)
+          Dep.target = null
+        } else {
+          if (Dep.target && deps.indexOf(Dep.target) === -1){
+            deps.push(Dep.target)
+          }
+        }
+
+        return val
+      },
+      set (newVal) {
+        val = newVal
+
+        if (deps.length) {
+          deps.forEach(dep => notify(dep))
+        }
+        notify(key)
+      }
     })
   }
 
   function observeData (obj) {
     for (let key in obj) {
       if (obj.hasOwnProperty(key)) {
-        const isNested = (typeof obj[key] === 'object' && !obj[key].length && !obj[key].deps)
-        if (isNested) {
-          observeData(obj[key])
+        if (typeof obj[key] === 'function') {
+          makeReactive(obj, key, obj[key])
         } else {
           makeReactive(obj, key)
         }
@@ -48,120 +66,59 @@ function Seer (initObj) {
     parseDOM(document.body, obj)
   }
 
-  function makeReactive (obj, key) {
-    let hasDeps = !!obj[key].deps
-    let property = obj[key]
-    let val
-
-    if (hasDeps) {
-      for (const dep of obj[key].deps) {
-        observe(dep, () => notify(key))
-      }
-    } else {
-      val = obj[key]
-    }
-
-    // This could be merged with makeReactive
-    Object.defineProperty(obj, key, {
-      get () {
-        return hasDeps
-          ? property.get.call(obj)
-          : val
-      },
-      set (newVal) {
-        if (hasDeps) {
-          return property.set.call(obj, newVal)
-        } else {
-          val = newVal
-          notify(key)
-        }
-      }
-    })
-
-
-    // Object.defineProperty(obj, key, {
-    //   get () {
-    //     return val
-    //   },
-    //   set (newVal) {
-    //     val = newVal
-    //     notify(key)
-    //   }
-    // })
+  function syncNode (node, observable, property) {
+    node.textContent = observable[property]
+    observe(property, () => node.textContent = observable[property])
   }
-  //
-  // function observeDeps (obj, key, get, set) {
-  //   for (const dep of obj[key].deps) {
-  //     observe(dep, () => notify(key))
-  //   }
-  //   // This could be merged with makeReactive
-  //   Object.defineProperty(initObj.data, key, {
-  //     get () {
-  //       return get.call(initObj.data)
-  //     },
-  //     set (newVal) {
-  //       return set.call(initObj.data, newVal)
-  //     }
-  //   })
-  // }
-  //
-  // function observeComputed (obj) {
-  //   for (let key in obj) {
-  //     if (obj.hasOwnProperty(key)) {
-  //       observeDeps(obj, key, obj[key].get, obj[key].set)
-  //     }
-  //   }
-  // }
+
+  function parseDOM (node, observable) {
+    const nodes = document.querySelectorAll('[s-text]')
+
+    nodes.forEach((node) => {
+      syncNode(node, observable, node.attributes['s-text'].value)
+    })
+  }
 }
 
-
-const App = new Seer({
-  data: {
-    // Currently supports only primitive values
-    // TODO: Support arrays and nested objects
-    value: 1,
-    multipleValue: 1,
-    firstName: 'Jon',
-    lastName: 'Snow',
-    reversedName: {
-      deps: ['fullName'],
-      get () {
-        return this.fullName.split('').reverse().join('')
-      }
-    },
-    fullName: {
-      deps: ['firstName', 'lastName'],
-      get () {
-        return this.firstName + ' ' + this.lastName
-      },
-      set (fullName) {
-        fullName = fullName.split(' ')
-        this.firstName = fullName[0] || ''
-        this.lastName = fullName[1] || ''
-      }
-    },
-    reversedNameWithLastName: {
-      deps: ['reversedName', 'lastName'],
-      get () {
-        return this.reversedName + ' ' + this.lastName
-      }
-    }
+const App = Seer({
+  title: 'Game of Thrones',
+  firstName: 'Jon',
+  lastName: 'Snow',
+  firstName2: 'Sansa',
+  lastName2: 'Stark',
+  age: 25,
+  ageError: '',
+  items: 0,
+  gender: 'male',
+  fullName () {
+    return this.gender === 'male'
+      ? 'Mr ' + this.firstName + ' ' + this.lastName
+      : 'Ms ' + this.firstName2 + ' ' + this.lastName2
+  },
+  fullNameLength () {
+    const length = this.fullName.length - 4
+    return length
+      ? length
+      : 'Full name not set'
   }
 })
 
-// To subscribe and react to changes made to the reactive App object:
-// App.observe('firstName', () => console.log(App.data.firstName))
-// App.observe('lastName', () => console.log(App.data.lastName))
-
-// To access or change the reactive data simply do:
-// App.data.firstName = 'Sansa'
-// App.data.lastName = 'Stark'
-
-function increment () {
-  App.data.value++
-  App.data.multipleValue = App.data.value * App.data.value
+function updateText (property, e) {
+	App.data[property] = e.target.value
 }
 
-function updateInput (target, event) {
-  App.data[target] = event.target.value
+function resetTitle () {
+	App.data.title = "Game of Thrones"
+}
+
+function updateName (event) {
+  App.data.firstName = event.target.value.split("").reverse().join("")
+}
+
+App.observe('age', () => {
+  App.data.ageError = parseInt(App.data.age) > 18 ? '' : 'Too young to watch'
+})
+
+function addToCart () {
+  App.data.items++
 }
